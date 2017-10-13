a37f405a8461eeb44dfc9043a070ddec8e7c5ede
Sam Rudd <samdrudd@gmail.com>
2017-10-12 19:18:41 -0700

    added get recipe/:id

--- app/routes/recipe_routes.js
+++ app/routes/recipe_routes.js
@@ -1,7 +1,30 @@
-module.exports = function(app, db) {
-	app.post('/recipe', (req, res) => {
+var ObjectID = require('mongodb').ObjectID;
 
-		console.log(req.body);
+module.exports = function(app, db) {
+
+	app.get('/recipe/:id', (req, res) => {
+		try
+		{
+			var details = { "_id": new ObjectID(req.params.id) };
+		}
+		catch(err)
+		{
+			res.status(400).send( { "error": "Invalid ID" } );
+			return;
+		}
+
+		db.collection('recipes').findOne(details, (err, item) => {
+			if (err)
+				res.send( {"error" : err });
+			else
+				if (item === null)
+					res.status(404).send({ "error" : "Recipe not found" } );
+				else
+					res.send(item);
+		});
+	});
+
+	app.post('/recipe', (req, res) => {
 
 		if (req.body.name === undefined || req.body.ingredients === undefined || req.body.instructions === undefined) 
 			res.status(400).send( {"error" : "Malformed object"} );
