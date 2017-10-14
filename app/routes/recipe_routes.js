3824662417983fcf4ca0be8bde63c214933fed9a
Sam Rudd <samdrudd@gmail.com>
2017-10-13 20:50:28 -0700

    Added PUT /recipes/:id

--- app/routes/recipe_routes.js
+++ app/routes/recipe_routes.js
@@ -2,15 +2,31 @@ var ObjectID = require('mongodb').ObjectID;
 
 module.exports = function(app, db) {
 
+	app.post('/recipes', (req, res) => {
+		if (req.body.name === undefined || req.body.ingredients === undefined || req.body.instructions === undefined) 
+			res.status(400).send( {"error" : "Malformed object"} );
+		else if (req.body.name.trim() === "" || req.body.ingredients.length <= 0 || req.body.instructions.trim() == "")
+			res.status(400).send( {"error" : "Empty name, ingredients, or instructions"} );
+		else {		
+			db.collection('recipes').insert( req.body , (err, result) => {
+				if (err)
+					res.status(500).send( {"error" : err} );
+				else
+					res.send(result.ops[0]);
+			});
+		}
+	});
+
+
 	app.get('/recipes/:id', (req, res) => {
 		try {
-			var details = {"_id" : new ObjectID(req.params.id)};
+			var id = {"_id" : new ObjectID(req.params.id)};
 		} catch(err) {
 			res.status(400).send( {"error" : "Invalid ID"} );
 			return;
 		}
 
-		db.collection('recipes').findOne(details, (err, item) => {
+		db.collection('recipes').findOne(id, (err, item) => {
 			if (err)
 				res.status(500).send( {"error" : err} );
 			else
@@ -31,19 +47,21 @@ module.exports = function(app, db) {
 	
 	});
 
-	app.post('/recipes', (req, res) => {
-		if (req.body.name === undefined || req.body.ingredients === undefined || req.body.instructions === undefined) 
-			res.status(400).send( {"error" : "Malformed object"} );
-		else if (req.body.name.trim() === "" || req.body.ingredients.length <= 0 || req.body.instructions.trim() == "")
-			res.status(400).send( {"error" : "Empty name, ingredients, or instructions"} );
-		else {		
-			db.collection('recipes').insert( req.body , (err, result) => {
-				if (err)
-					res.status(500).send( {"error" : err} );
-				else
-					res.send(result.ops[0]);
-			});
+	app.put('/recipes/:id', (req, res) => {
+		try {
+			var id = {"_id" : new ObjectID(req.params.id)};
+		} catch(err) {
+			res.status(400).send( {"error" : "Invalid ID"} );
+			return;
 		}
+
+
+		db.collection('recipes').update(id, req.body, (err, result) => {
+			if (err)
+				res.status(500).send( {"error" : err} );
+			else
+				res.send( req.body );
+		});
 	});
 
 	app.delete('/recipes/:id', (req, res) => {
